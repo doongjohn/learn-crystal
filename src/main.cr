b7cb447129ef10b8b7bd8eed9dd934bf7b1765f4
doongjohn <doongjohn@gmail.com>
2023-07-20 17:16:13 +0900

    update

--- src/main.cr
+++ src/main.cr
@@ -1,12 +1,14 @@
 # modules: https://codingpackets.com/blog/crystal-notes-modules/
 
 module Learn::Crystal # <-- module is similar to csharp `static class`
+                      #     but also can be used as a mixin via `include`
   # Variable
   x = 10_u32
   s = "this is string"
   puts typeof(s) # <-- String
   s = 1 # <-- variable shadowing
   puts typeof(s) # <-- Int32
+  puts ""
 
   # truthy & falsy
   # `nil`, `false`, and `null pointers` are falsy
@@ -14,10 +16,10 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   if 0
     puts "truthy"
   end
-
   unless false
     puts "unless cond == if !(cond)"
   end
+  puts ""
 
   # Flow control statements don't make a new scope in Crystal. This is inherited from Ruby.
   # https://github.com/crystal-lang/crystal/issues/9588
@@ -27,6 +29,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
     a = "hello"
   end
   puts typeof(a) # => Int32 | String
+  puts ""
 
   # Constant
   HELLO = "hello"
@@ -41,7 +44,9 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
 
   hi # <-- this is a function call
      #     `()` is optional
+  puts ""
 
+  # Module
   module Hi
     # module can be nested
     HELLO = "hello"
@@ -53,6 +58,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
 
   p! Hi::HELLO
   p! Hi.say_hi
+  puts ""
 
   def self.run(& : Int32 -> Int32)
     #          ^ --> this is a block
@@ -63,30 +69,61 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   run do |number|
     number + 1
   end
+  puts ""
 
   # String
   puts "s = #{s}"
   puts %q(s = \n #{s})
   puts %(s = \n "#{s}")
   puts %w(this is string) # <-- split string by space
+  puts ""
 
   # Array
   a = [2, 3] of UInt32
   int_arr = [1, *a, 4] of UInt32
   int_arr << 5
   puts typeof(int_arr)
+  puts ""
 
   int_arr.each do |elem|
     puts elem
   end
+  puts ""
+
   int_arr[...2].each do |elem|
     puts elem
   end
+  puts ""
 
   head, *rest, tail = [1, 2, 3, 4, 5]
   puts "head: #{head}"
   puts "rest: #{rest}"
   puts "tail: #{tail}"
+  puts ""
+
+  # Loop
+  3.times do |i|
+    p! i
+  end
+
+  puts ""
+  res = (1..5).each do |i|
+    if i == 2
+      next # <-- continue
+    end
+    p! i
+    break i if i == 5
+    #     ^ ^^^^^^^^^
+    #     │ └─> break condition
+    #     └─> break with value
+  end
+  p! res
+  puts ""
+
+  res = (3..6).each_with_index do |val, i|
+    p! ({i, val})
+  end
+  puts ""
 
   # Hash (key value pair)
   people = {
@@ -103,6 +140,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   people["I'm"] &&= "cool"
   #             ^^^ <-- set only when the key is present
   puts %(&&= I'm a #{people["I'm"]})
+  puts ""
 
   # Tuple
   print "Enter a number: "
@@ -117,14 +155,14 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
       puts tuple[i]
     end
   end
+  puts ""
 
   # Class
   class Human
     # property name : String
     getter age : Int32 = 10
 
-    def initialize(name : String) # <-- this is the constructor
-      @name = name
+    def initialize(@name : String) # <-- this is the constructor
     end
 
     def name=(value)
@@ -141,6 +179,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   human = Human.new "John"
   human.age = 11
   human.name = "Tom"
+  puts ""
 
   # Multiple assignment
   a = 1
@@ -153,6 +192,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   puts name
   puts hi
   puts wow
+  puts ""
 
   # Case
   num = 10
@@ -184,6 +224,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   when {Int32, Int32}
     puts "not match"
   end
+  puts ""
 
   # Enum
   enum SomeEnum
@@ -195,6 +236,7 @@ module Learn::Crystal # <-- module is similar to csharp `static class`
   e = SomeEnum::HelloFriends
   p! e == SomeEnum::HelloFriends
   p! e.hello_friends?
+  puts ""
 
   # shell command
   p! `echo foo`  # => "foo\n"
